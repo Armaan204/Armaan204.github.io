38b2de8e747614d3c8b638569c5090134d451c0b
Armaan204 <armaanrehmanshah1@gmail.com>
2024-05-22 21:31:57 -0400

    Edited the landing hero page

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Armaan Rehman Shah.</h2>;
+  const three = <h3 className="big-heading">I decode the language of data.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a 2nd year Statistics student at UofT, specializing in machine learning and data
+        science. Currently, I’m in the midst of a Data Analyst Co-op Internship at
+        <a href="https://www.geotab.com/" target="_blank" rel="noreferrer">
+          Geotab
         </a>
         .
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="www.linkedin.com/in/armaanrs" target="_blank" rel="noreferrer">
+      Check out my LinkedIn!
     </a>
   );
 
