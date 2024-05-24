ec2f9267bf491f40adb0e8f487f6e2c4fec7c9f8
Armaan204 <armaanrehmanshah1@gmail.com>
2024-05-23 22:14:03 -0400

    Updated favicon, head logo, head title, and side icons and email

--- src/components/icons/codepen.js
+++ src/components/icons/codepen.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 const IconCodepen = () => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    role="img"
+    fill="#000000"
+    width="800px"
+    height="800px"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-codepen">
-    <title>CodePen</title>
-    <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
-    <line x1="12" y1="22" x2="12" y2="15.5"></line>
-    <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
-    <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
-    <line x1="12" y1="2" x2="12" y2="8.5"></line>
+    role="img"
+    xmlns="http://www.w3.org/2000/svg">
+    <path d="M6.002 1.61 0 12.004 6.002 22.39h11.996L24 12.004 17.998 1.61zm1.593 4.084h3.947c3.605 0 6.276 1.695 6.276 6.31 0 4.436-3.21 6.302-6.456 6.302H7.595zm2.517 2.449v7.714h1.241c2.646 0 3.862-1.55 3.862-3.861.009-2.569-1.096-3.853-3.767-3.853z" />
   </svg>
 );
 
--- src/config.js
+++ src/config.js
@@ -1,26 +1,22 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'armaanrehmanshah1@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/Armaan204',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://www.instagram.com/armaanshah__',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/armaanrs',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://devpost.com/armaanrehmanshah1',
     },
   ],
 
