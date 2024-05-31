a565a54360d79ac776cb5d58212b84462de6375e
Armaan204 <armaanrehmanshah1@gmail.com>
2024-05-30 23:34:32 -0400

    finished about me section and whats next section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Google BigQuery',
+    'Apache Airflow',
+    'Pandas',
+    'SQL',
+    'Apache Superset',
+    'Google Apps Script',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,36 +142,31 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Armaan and I'm passionate about data and its potential to solve
-              complex problems.
+              Hello! My name is Armaan and I'm passionate about data and its potential to drive
+              innovation, reveal insights and pave the way for breakthroughs.
             </p>
 
             <p>
-              Digest this: 90% of the world's data has been generated in the past{' '}
-              <a href="https://www.statista.com/statistics/871513/worldwide-data-created/">
-                two years alone
-              </a>{' '}
-              — and the number just keeps increasing at a faster rate!
+              I have also dabbled in software development, building platforms and apps to solve
+              real-life complex problems.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              As an international student who has lived in third world countries, this drive to make
+              positive change has always been within me.
+            </p>
+            <p>
+              From creating a{' '}
+              <a href="https://github.com/Armaan204/Lab-Scheduler">lab reservation application</a>{' '}
+              for my highschool to helping the{' '}
+              <a href="https://www.geotab.com/">world's leading telematics company</a> optimize
+              their data, I constantly strive to learn new technologies fast and apply them to make
+              a change.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              In my free time I like <a href="https://utsc-chessclub.weebly.com/">playing chess</a>,{' '}
+              hanging out with friends, and gaming.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
